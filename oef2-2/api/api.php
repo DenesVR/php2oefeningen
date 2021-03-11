d10c50bf82ad0c8f92938691597b84816e9f1dc9
DenesVR <denesvanroy@gmail.com>
2021-03-11 16:25:02 +0100

    Oef 2.2

--- oef2-2/api/api.php
+++ oef2-2/api/api.php
@@ -3,6 +3,21 @@ $public_access =  true;
 require_once "../lib/autoload.php";
 header("Access-Control-Allow-Origin: 'https://gf.dev'");
 
+
+header("Access-Control-Allow-Credentials 'true'");
+
+
+//Allow GET, POST, PUT, DELETE, OPTIONS http methods
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+//Allow some types of headers
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
+//Set response content type and character set
+header("Content-Type: application/json; charset=UTF-8");
+
+
+
 $method = $_SERVER['REQUEST_METHOD'];
 $request_uri = $_SERVER['REQUEST_URI'];
 
@@ -25,7 +40,7 @@ if ( $method == "GET" AND $request_part == "btwcodes" )
 {
     $sql = "select * from eu_btw_codes";
     // ... execute $sql
-    print json_encode( [ "msg" => $sql ] ) ; //normaal zou je hier alle spelers teruggeven
+    print json_encode( [ "msg" => $sql ] ) ;
 }
 
 //GET btwcode: één btwcode geven
@@ -33,7 +48,7 @@ if ( $method == "GET" AND $request_part == "btwcode" )
 {
     $sql = "select * from eu_btw_codes where eub_id=$id";
     // ... execute $sql
-    print json_encode( [ "msg" => $sql ] ) ; //normaal zou je hier één speler teruggeven
+    print json_encode( [ "msg" => $sql ] ) ;
 }
 
 //POST btwcodes: een btwcode toevoegen
@@ -43,7 +58,7 @@ if ( $method == "POST" AND $request_part == "btwcodes"  )
     $sql = "INSERT INTO eu_btw_codes SET eub_code='$code' ";
     // ... execute $sql
     http_response_code(201);
-    print json_encode( [ "msg" => $sql ] ) ; //normaal zou je hier een OK teruggeven
+    print json_encode( [ "msg" => $sql ] ) ;
 }
 
 //PUT btwcode: een btwcode updaten
@@ -54,7 +69,7 @@ if ( $method == "PUT" AND $request_part == "btwcode" )
 
     $sql = "UPDATE eu_btw_codes SET eub_code='$newdata' WHERE eub_id=$id";
     // ... execute $sql
-    print json_encode( [ "msg" => $sql ] ) ; //normaal zou je hier een OK teruggeven
+    print json_encode( [ "msg" => $sql ] ) ;
 }
 
 //DELETE btwcode: een btwcode verwijderen
@@ -62,7 +77,7 @@ if ( $method == "DELETE" AND $request_part == "btwcode" )
 {
     $sql = "DELETE FROM eu_btw_codes WHERE eub_id=$id";
     // ... execute $sql
-    print json_encode( [ "msg" => $sql ] ) ; //normaal zou je hier een OK teruggeven
+    print json_encode( [ "msg" => $sql ] ) ;
 }
 
 ?>
